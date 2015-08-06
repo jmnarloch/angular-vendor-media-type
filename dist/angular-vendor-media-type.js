389a6f356dcd47c26b3d05722dae46a7d41c1f9e
Jakub Narloch <jmnarloch@gmail.com>
2015-08-06 20:48:02 +0200

    Extracted AcceptHeaderProcessor

--- dist/angular-vendor-media-type.js
+++ dist/angular-vendor-media-type.js
@@ -4,14 +4,59 @@
       return $httpProvider.interceptors.push('httpRequestInterceptorVendorMimeType');
     }
   ]).provider('httpRequestInterceptorVendorMimeType', function() {
-    var HttpRequestInterceptorVendorMimeTypeProvider;
+    var AcceptHeaderProcessor, HttpRequestInterceptorVendorMimeTypeProvider;
+    AcceptHeaderProcessor = (function() {
+      var SEPARATOR;
+
+      SEPARATOR = ',';
+
+      function AcceptHeaderProcessor(config) {
+        this.config = angular.extend({
+          mimeTypePattern: /([\s\w\d+-\/\/*.]+)(:?;[\s\w\d+-\/\/*.=])?/
+        }, config);
+      }
+
+      AcceptHeaderProcessor.prototype.process = function(header) {
+        var headerMimeTypes;
+        headerMimeTypes = this.extractMimeTypes(header);
+        if (this.matchesMimeTypes(headerMimeTypes)) {
+          return this.config.vendorMimeType;
+        }
+        return header;
+      };
+
+      AcceptHeaderProcessor.prototype.extractMimeTypes = function(header) {
+        var mimeTypePattern;
+        mimeTypePattern = this.config.mimeTypePattern;
+        return header.split(SEPARATOR).map(function(mime) {
+          var match;
+          if (!mimeTypePattern.test(mime)) {
+            return mime;
+          }
+          match = mimeTypePattern.exec(mime);
+          return match[1].trim();
+        });
+      };
+
+      AcceptHeaderProcessor.prototype.matchesMimeTypes = function(headerMimeTypes) {
+        var i, len, mimeType, ref;
+        ref = this.config.mimeTypes;
+        for (i = 0, len = ref.length; i < len; i++) {
+          mimeType = ref[i];
+          if (headerMimeTypes.indexOf(mimeType) > -1) {
+            return true;
+          }
+        }
+        return false;
+      };
+
+      return AcceptHeaderProcessor;
+
+    })();
     HttpRequestInterceptorVendorMimeTypeProvider = (function() {
-      function HttpRequestInterceptorVendorMimeTypeProvider() {
-        this.paths = [/.*api.*/];
-        this.mimeTypes = ['text/xml', 'application/xml', 'application/json'];
-        this.mimeTypePattern = /([\s\w\d+-\/\/*.]+)(:?;[\s\w\d+-\/\/*.=])?/;
-        this.vendorMimeType = '';
-        this.vendor;
+      function HttpRequestInterceptorVendorMimeTypeProvider(paths, mimeTypes) {
+        this.paths = paths;
+        this.mimeTypes = mimeTypes;
       }
 
       HttpRequestInterceptorVendorMimeTypeProvider.prototype.setVendorMimeType = function(vendorMimeType1) {
@@ -26,26 +71,21 @@
         this.mimeTypes = mimeTypes1;
       };
 
-      HttpRequestInterceptorVendorMimeTypeProvider.prototype.withVendor = function(vendor) {
-        this.vendor = vendor;
+      HttpRequestInterceptorVendorMimeTypeProvider.prototype.withVendor = function(vendor1) {
+        this.vendor = vendor1;
       };
 
       HttpRequestInterceptorVendorMimeTypeProvider.prototype.$get = function($q) {
-        var extractMimeTypes, matchesMimeTypes, matchesPath, mimeTypePattern, mimeTypes, paths, vendorMimeType;
+        var matchesPath, mimeTypes, paths, processor, vendor, vendorMimeType;
         paths = this.paths;
         mimeTypes = this.mimeTypes;
-        mimeTypePattern = this.mimeTypePattern;
         vendorMimeType = this.vendorMimeType;
-        extractMimeTypes = function(header) {
-          return header.split(',').map(function(mime) {
-            var match;
-            if (!mimeTypePattern.test(mime)) {
-              return mime;
-            }
-            match = mimeTypePattern.exec(mime);
-            return match[1].trim();
-          });
-        };
+        vendor = this.vendor;
+        processor = new AcceptHeaderProcessor({
+          mimeTypes: mimeTypes,
+          vendorMimeType: vendorMimeType,
+          vendor: vendor
+        });
         matchesPath = function(url) {
           var i, len, path;
           for (i = 0, len = paths.length; i < len; i++) {
@@ -56,24 +96,12 @@
           }
           return false;
         };
-        matchesMimeTypes = function(acceptMimeTypes) {
-          var i, len, mimeType;
-          for (i = 0, len = mimeTypes.length; i < len; i++) {
-            mimeType = mimeTypes[i];
-            if (acceptMimeTypes.indexOf(mimeType) > -1) {
-              return true;
-            }
-          }
-          return false;
-        };
         return {
           'request': function(config) {
-            var acceptMimeTypes;
-            if (vendorMimeType) {
-              acceptMimeTypes = extractMimeTypes(config.headers.Accept);
-              if (matchesPath(config.url) && matchesMimeTypes(acceptMimeTypes)) {
-                config.headers.Accept = vendorMimeType;
-              }
+            var value;
+            if ((vendorMimeType || angular.isDefined(vendor)) && matchesPath(config.url)) {
+              value = processor.process(config.headers.Accept);
+              config.headers.Accept = value;
             }
             return config || $q.when(config);
           }
@@ -83,7 +111,7 @@
       return HttpRequestInterceptorVendorMimeTypeProvider;
 
     })();
-    return new HttpRequestInterceptorVendorMimeTypeProvider();
+    return new HttpRequestInterceptorVendorMimeTypeProvider([/.*/], ['text/xml', 'application/xml', 'application/json']);
   });
 
 }).call(this);
--- src/coffee/angular-vendor-media-type.coffee
+++ src/coffee/angular-vendor-media-type.coffee
@@ -4,14 +4,45 @@ angular.module 'ngVendorMimeType', []
 ]
 .provider('httpRequestInterceptorVendorMimeType', ->
 
+  class AcceptHeaderProcessor
+
+    SEPARATOR = ','
+
+    constructor: (config) ->
+      @config = angular.extend((
+        mimeTypePattern: /([\s\w\d+-//*.]+)(:?;[\s\w\d+-//*.=])?/,
+      ), config)
+
+    process: (header) ->
+      headerMimeTypes = @extractMimeTypes(header)
+
+      if @matchesMimeTypes(headerMimeTypes)
+        return @config.vendorMimeType
+
+      return header
+
+    extractMimeTypes: (header) ->
+      mimeTypePattern = @config.mimeTypePattern
+
+      return header.split(SEPARATOR).map((mime) ->
+        if not mimeTypePattern.test mime
+          return mime
+
+        match = mimeTypePattern.exec mime
+        return match[1].trim()
+      )
+
+    matchesMimeTypes: (headerMimeTypes) ->
+      for mimeType in @config.mimeTypes
+        if headerMimeTypes.indexOf(mimeType) > -1
+          return true
+      return false
+
   class HttpRequestInterceptorVendorMimeTypeProvider
 
-    constructor: ->
-      @paths = [/.*api.*/]
-      @mimeTypes = ['text/xml', 'application/xml', 'application/json']
-      @mimeTypePattern = /([\s\w\d+-//*.]+)(:?;[\s\w\d+-//*.=])?/
-      @vendorMimeType = ''
-      @vendor
+    constructor: (paths, mimeTypes) ->
+      @paths = paths
+      @mimeTypes = mimeTypes
 
     setVendorMimeType: (@vendorMimeType) ->
     matchingRequests: (@paths) ->
@@ -21,17 +52,14 @@ angular.module 'ngVendorMimeType', []
     $get: ($q) ->
       paths = @paths
       mimeTypes = @mimeTypes
-      mimeTypePattern = @mimeTypePattern
       vendorMimeType = @vendorMimeType
+      vendor = @vendor
 
-      extractMimeTypes = (header) ->
-        return header.split(',').map((mime) ->
-          if not mimeTypePattern.test mime
-            return mime
-
-          match = mimeTypePattern.exec mime
-          return match[1].trim()
-        )
+      processor = new AcceptHeaderProcessor(
+        mimeTypes: mimeTypes
+        vendorMimeType: vendorMimeType
+        vendor: vendor
+      )
 
       matchesPath = (url) ->
         for path in paths
@@ -40,22 +68,15 @@ angular.module 'ngVendorMimeType', []
 
         return false
 
-      matchesMimeTypes = (acceptMimeTypes) ->
-        for mimeType in mimeTypes
-          if acceptMimeTypes.indexOf(mimeType) > -1
-            return true
-        return false
-
       return (
         'request': (config) ->
-          if vendorMimeType
+          if (vendorMimeType || angular.isDefined(vendor)) && matchesPath(config.url)
 
-            acceptMimeTypes = extractMimeTypes(config.headers.Accept)
-            if matchesPath(config.url) && matchesMimeTypes(acceptMimeTypes)
-              config.headers.Accept = vendorMimeType
+            value = processor.process(config.headers.Accept)
+            config.headers.Accept = value
 
           return config || $q.when(config)
       )
 
-  return new HttpRequestInterceptorVendorMimeTypeProvider()
+  return new HttpRequestInterceptorVendorMimeTypeProvider([/.*/], ['text/xml', 'application/xml', 'application/json'])
 )
